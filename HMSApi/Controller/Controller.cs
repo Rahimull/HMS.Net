0e0499fe49948f08cf10ab614845790abb0ca799
Rahimull <rahimullahpashay@gmail.com>
2026-03-28 16:26:11 +0430

    Initial commit

---
+++ HMSApi/Controller/Controller.cs
@@ -0,0 +1,10 @@
+// [ApiController]
+// [Route("api/[controller]")]
+// public class TestController : ControllerBase
+// {
+//     [HttpGet]
+//     public string Get()
+//     {
+//         return "Hello API";
+//     }
+// }
